9057af84a994fc8662344624f5ec872615adb9bc
Chen Zhiqiang <1921437884@qq.com>
2023-03-15 10:45:32 +0800

    chore: 代码优化

--- src/modbusRTU/master/master-rtu.ino
+++ src/modbusRTU/master/master-rtu.ino
@@ -1,73 +1,123 @@
 #include <ModbusRTU.h>
+
+#if CONFIG_IDF_TARGET_ESP32S3
+#define RX_PIN 3
+#define TX_PIN 2
+#define RGB_PIN 48
+HardwareSerial S(1);
+
+#elif CONFIG_IDF_TARGET_ESP32C3
 #include <SoftwareSerial.h>
+#define RX_PIN 1
+#define TX_PIN 0
+#define RGB_PIN 8
+SoftwareSerial S;
 
-#define BUTTON_PIN 9 // Boot button pin
+#endif
+
+#define EN_PIN 1
+
 #define SLAVE_ID 1
-
-SoftwareSerial S(1, 0);
 
 ModbusRTU mb;
 
-bool cbWrite(Modbus::ResultCode event, uint16_t transactionId, void* data) {
-  Serial.printf_P("Request result: 0x%02X, Mem: %d\n", event, ESP.getFreeHeap());
+Modbus::ResultCode code;
+
+bool cbHandle(Modbus::ResultCode event, uint16_t transactionId, void* data) {
+  Serial.printf_P("Request result: 0x%02X ...\n", event);
+  code = event;
 
   return true;
 }
 
 void setup() {
   Serial.begin(115200);
-  S.begin(9600, SWSERIAL_8N1);
-  mb.begin(&S, 10);
-  
-  mb.master();
 
-  attachInterrupt(digitalPinToInterrupt(BUTTON_PIN), pin_ISR, FALLING);
+#if CONFIG_IDF_TARGET_ESP32S3
+  S.begin(9600, SERIAL_8N1, RX_PIN, TX_PIN);
+#elif CONFIG_IDF_TARGET_ESP32C3
+  S.begin(9600, SWSERIAL_8N1, RX_PIN, TX_PIN);
+#endif
+
+  mb.begin(&S, EN_PIN);
+  mb.master();
 }
 
-bool coils[10];
+bool coils[5];
 uint16_t res[1];
 
-bool isUpdateHreg = false;
-// bool isUpdateCoil = false;
-
-void pin_ISR()
-{
-  Serial.println("Interrupt writeHreg");
-  mb.writeHreg(SLAVE_ID, 0, 10);
-  isUpdateHreg = true;
-  // mb.writeCoil(SLAVE_ID, 0, false);
-  // isUpdateCoil = true;
-}
-
 void loop() {
-  if (!mb.slave() || isUpdateHreg) {
-    mb.readHreg(SLAVE_ID, 0, res, 1, cbWrite);
-    neopixelWrite(8, RGB_BRIGHTNESS, 0, 0);
+  if (!mb.slave()) {
+    mb.readHreg(SLAVE_ID, 0, res, 1, cbHandle);
     while (mb.slave()) {
-      isUpdateHreg = false;
       mb.task();
       delayMicroseconds(100);
     }
-    neopixelWrite(8, 0, 0, 0);
-    Serial.printf("hreg value: %d\n", res[0]);
+    if (code == Modbus::EX_SUCCESS) {
+      neopixelWrite(RGB_PIN, 0, RGB_BRIGHTNESS, 0);
+      Serial.printf("read hreg value: %d\n", res[0]);
+    } else {
+      neopixelWrite(RGB_PIN, RGB_BRIGHTNESS, 0, 0);
+    }
+    delay(100);
+    neopixelWrite(RGB_PIN, 0, 0, 0);
   }
-
+  
   delay(1000);
 
   if (!mb.slave()) {
-    mb.readCoil(SLAVE_ID, 0, coils, 10);
-    neopixelWrite(8, RGB_BRIGHTNESS, 0, 0);
+    mb.readCoil(SLAVE_ID, 0, coils, 5, cbHandle);
     while (mb.slave()) {
       mb.task();
       delayMicroseconds(100);
     }
-    neopixelWrite(8, 0, 0, 0);
-    Serial.printf("coil value: ");
-    for (int i = 0; i < 10; i++) {
-      Serial.printf("%d ", coils[i]);
+    if (code == Modbus::EX_SUCCESS) {
+      neopixelWrite(RGB_PIN, 0, RGB_BRIGHTNESS, 0);
+      Serial.printf("coil value: ");
+      for (int i = 0; i < 5; i++) {
+        Serial.printf("%d ", coils[i]);
+      }
+      Serial.println();
+    } else {
+      neopixelWrite(RGB_PIN, RGB_BRIGHTNESS, 0, 0);
     }
-    Serial.println();
+    delay(100);
+    neopixelWrite(RGB_PIN, 0, 0, 0);
   }
 
   delay(1000);
+
+  // if (!mb.slave()) {
+  //   mb.writeHreg(SLAVE_ID, 0, 10, cbHandle);
+  //   while (mb.slave()) {
+  //     mb.task();
+  //     delayMicroseconds(100);
+  //   }
+  //   if (code == Modbus::EX_SUCCESS) {
+  //     neopixelWrite(RGB_PIN, 0, RGB_BRIGHTNESS, 0);
+  //     Serial.printf("write hreg offset: %d, value: %d\n", 0, 10);
+  //   } else {
+  //     neopixelWrite(RGB_PIN, RGB_BRIGHTNESS, 0, 0);
+  //   }
+  //   delay(100);
+  //   neopixelWrite(RGB_PIN, 0, 0, 0);
+  // }
+
+  // delay(1000);
+
+  // if (!mb.slave()) {
+  //   mb.writeCoil(SLAVE_ID, 0, false, cbHandle);
+  //   while (mb.slave()) {
+  //     mb.task();
+  //     delayMicroseconds(100);
+  //   }
+  //   if (code == Modbus::EX_SUCCESS) {
+  //     neopixelWrite(RGB_PIN, 0, RGB_BRIGHTNESS, 0);
+  //     Serial.printf("write coil offset: %d, value: %d\n", 0, 0);
+  //   } else {
+  //     neopixelWrite(RGB_PIN, RGB_BRIGHTNESS, 0, 0);
+  //   }
+  //   delay(100);
+  //   neopixelWrite(RGB_PIN, 0, 0, 0);
+  // }
 }
--- src/modbusRTU/slave/slave-rtu.ino
+++ src/modbusRTU/slave/slave-rtu.ino
@@ -1,36 +1,50 @@
 #include <ModbusRTU.h>
-#include <SoftwareSerial.h>
 
+#if CONFIG_IDF_TARGET_ESP32S3
+#define RX_PIN 3
+#define TX_PIN 2
+HardwareSerial S(1);
+
+#elif CONFIG_IDF_TARGET_ESP32C3
+#include <SoftwareSerial.h>
+#define RX_PIN 1
+#define TX_PIN 0
+SoftwareSerial S;
+
+#endif
+
+#define EN_PIN 1
 #define REGN 0
 #define SLAVE_ID 1
 
-SoftwareSerial S(1, 0);
-
 ModbusRTU mb;
 
-uint16_t getHregCb(TRegister *reg, uint16_t val) {
-  // neopixelWrite(8, 0, RGB_BRIGHTNESS, 0);
-  Serial.printf("reg val: %d\n", val);
+uint16_t getRegCb(TRegister *reg, uint16_t val) {
+  int regType = reg->address.type;
+  uint16_t regAddress = reg->address.address;
+  
+  Serial.printf("Get RegType: %d, RegAddress: %d, RegVal: %d\n", regType, regAddress, val);
 
   return val;
 }
 
 void setup() {
   Serial.begin(115200);
-  S.begin(9600, SWSERIAL_8N1);
-  mb.begin(&S, 10);
 
+#if CONFIG_IDF_TARGET_ESP32S3
+  S.begin(9600, SERIAL_8N1, RX_PIN, TX_PIN);
+#elif CONFIG_IDF_TARGET_ESP32C3
+  S.begin(9600, SWSERIAL_8N1, RX_PIN, TX_PIN);
+#endif
+
+  mb.begin(&S, EN_PIN);
   mb.slave(SLAVE_ID);
 
-  mb.addHreg(REGN);
-  mb.Hreg(REGN, 100);
+  mb.addHreg(REGN, 100);
+  mb.addCoil(REGN, true, 5);
 
-  if (mb.Coil(0)) {
-    mb.removeCoil(0, 10);
-  }
-  mb.addCoil(0, true, 10);
-
-  mb.onGet(HREG(REGN), getHregCb);
+  mb.onGet(HREG(REGN), getRegCb);
+  mb.onGet(COIL(REGN), getRegCb);
 }
 
 void loop() {
