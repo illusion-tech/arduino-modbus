332ef873e29b4e11d4953b1fe7422fe9278cd9c3
Chen Zhiqiang <1921437884@qq.com>
2023-05-16 17:49:47 +0800

    feat: 添加移动 app modbus 通信

--- src/modbusRTU/slave-rtu-no-library/slave-rtu-no-library.ino
+++ src/modbusRTU/slave-rtu-no-library/slave-rtu-no-library.ino
@@ -24,7 +24,7 @@ struct ModbusFrame {
 };
 
 // 定义Modbus帧的最大长度
-#define MAX_FRAME_LENGTH 256
+#define MAX_FRAME_LENGTH 64
 
 // 定义从机地址
 #define SLAVE_ADDRESS 1
@@ -41,18 +41,28 @@ bool COIL[REG_NUM] = {0, 0, 0};
 // 定义继电器引脚
 uint16_t REPLAY_PINS[REG_NUM] = {2, 4, 5};
 
-// 定义串口通信的 RX TX 使能 引脚
+// 定义上位机通信的 RX TX 使能 引脚
 #define PC_RX_PIN 18
 #define PC_TX_PIN 17
-#define PC_ENABLE_PIN 21
-
+#define PC_EN_PIN 21
 HardwareSerial pcSerial(1);
+
+// 定义移动app端通信的 RX TX 使能 引脚
+#define APP_RX_PIN 48
+#define APP_TX_PIN 47
+#define APP_EN_PIN 38
+HardwareSerial appSerial(2);
 
 void setup() {
   Serial.begin(115200);
+  // 初始化串口1
   pcSerial.begin(115200, SERIAL_8N1, PC_RX_PIN, PC_TX_PIN);
-  pinMode(PC_ENABLE_PIN, OUTPUT);
-  digitalWrite(PC_ENABLE_PIN, LOW);  // 使能管脚低电平有效
+  pinMode(PC_EN_PIN, OUTPUT);
+  digitalWrite(PC_EN_PIN, LOW);
+  // 初始化串口2
+  appSerial.begin(115200, SERIAL_8N1, APP_RX_PIN, APP_TX_PIN);
+  pinMode(APP_EN_PIN, OUTPUT);
+  digitalWrite(APP_EN_PIN, LOW);
 
   for (int i = 0; i < REG_NUM; i++) {
     pinMode(REPLAY_PINS[i], OUTPUT);
@@ -64,11 +74,9 @@ void setup() {
 uint16_t crc16(uint8_t *buffer, uint16_t length) {
   uint16_t crc, temp, flag;
   crc = 0xFFFF;
-  for (uint8_t i = 0; i < length; i++)
-  {
+  for (uint8_t i = 0; i < length; i++) {
     crc ^= buffer[i];
-    for (uint8_t j = 1; j <= 8; j++)
-    {
+    for (uint8_t j = 1; j <= 8; j++) {
       flag = crc & 0x0001;
       crc >>= 1;
       if (flag)
@@ -132,7 +140,7 @@ void getResponseFrame(ModbusFrame *frame, uint8_t *response) {
 }
 
 // 发送响应帧
-void sendResponseFrame(ModbusFrame *frame) {
+void sendResponseFrame(HardwareSerial *serial, uint16_t en_pin, ModbusFrame *frame) {
   uint8_t response[MAX_FRAME_LENGTH];
   getResponseFrame(frame, response);
 
@@ -143,12 +151,12 @@ void sendResponseFrame(ModbusFrame *frame) {
   }
   Serial.println();
 
-  digitalWrite(PC_ENABLE_PIN, HIGH);  // 使能RS485发送
+  digitalWrite(en_pin, HIGH);  // 使能RS485发送
   for (int i = 0; i < 3 + response[2] + 2; i++) {
-    pcSerial.write(response[i]);
+    serial->write(response[i]);
   }
-  pcSerial.flush();
-  digitalWrite(PC_ENABLE_PIN, LOW);  // 禁用RS485发送
+  serial->flush();
+  digitalWrite(en_pin, LOW);  // 禁用RS485发送
 }
 
 /// @brief 设置寄存器的值
@@ -193,54 +201,69 @@ void setRelay(uint16_t offset, bool value) {
   digitalWrite(REPLAY_PINS[offset], value);
 }
 
+// 处理接收到的 buffer
+void handleBuffer(HardwareSerial *serial, uint16_t en_pin, uint8_t *buffer, uint16_t length) {
+  // 校验从机地址
+  if (buffer[0] != SLAVE_ADDRESS) return;
+  // 校验 CRC
+  uint16_t crc = crc16(buffer, length - 2);
+  if (crc != (buffer[length - 2] << 8 | buffer[length - 1])) return;
+
+  ModbusFrame frame;
+  getRequestFrame(&frame, buffer, length);
+
+  FunctionCode fcode = (FunctionCode)frame.fcode;
+  switch (fcode) {
+    // 读取线圈和保持寄存器，发送响应帧
+    case FunctionCode::READ_COILS:
+    case FunctionCode::READ_REGS:
+      sendResponseFrame(serial, en_pin, &frame);
+      break;
+    case FunctionCode::WRITE_COIL:
+    case FunctionCode::WRITE_COILS:
+    case FunctionCode::WRITE_REG:
+    case FunctionCode::WRITE_REGS:
+      setRegister(&frame, buffer);
+      break;
+  }
+}
+
+// 上位机串口接收缓冲区
+uint8_t pc_buffer[MAX_FRAME_LENGTH];
+uint16_t pc_buffer_index = 0;
+
+// app串口接收缓冲区
+uint8_t app_buffer[MAX_FRAME_LENGTH];
+uint16_t app_buffer_index = 0;
+
 void loop() {
   // 接收上位机 Modbus 帧
-  while (pcSerial.available()) {
-    uint8_t buffer[MAX_FRAME_LENGTH];
-    uint16_t length = 0;
+  if (pcSerial.available()) {
     while (pcSerial.available()) {
-      buffer[length++] = pcSerial.read();
-      if (length >= MAX_FRAME_LENGTH) {
-        break;
-      }
+      pc_buffer[pc_buffer_index++] = pcSerial.read();
+    }
+    
+    if (pc_buffer_index == 1 && pc_buffer[0] == 0x00) {
+      pc_buffer_index = 0;
+      return;
     }
 
-    if (length == 1 && buffer[0] == 0x00) {
-      Serial.println("接收到空帧");
-      continue;
+    handleBuffer(&pcSerial, PC_EN_PIN, pc_buffer, pc_buffer_index);
+    pc_buffer_index = 0;
+  }
+
+  // 接收 app Modbus 帧
+  if (appSerial.available()) {
+    while (appSerial.available()) {
+      app_buffer[app_buffer_index++] = appSerial.read();
     }
-    if (buffer[0] != SLAVE_ADDRESS) return;
-
-    Serial.print("receive:");
-    for (int i = 0; i < length; i++) {
-      Serial.print(buffer[i], HEX);
-      Serial.print(" ");
+    
+    if (app_buffer_index == 1 && app_buffer[0] == 0x00) {
+      app_buffer_index = 0;
+      return;
     }
-    Serial.println();
 
-    // 解析Modbus帧
-    ModbusFrame frame;
-    getRequestFrame(&frame, buffer, length);
-
-    // 如果地址不匹配，忽略此帧
-    if (frame.address != SLAVE_ADDRESS) return;
-
-    // 如果起始地址不匹配，忽略此帧
-    if (frame.startAddress != HREG_ADDRESS) return;
-
-    FunctionCode fcode = (FunctionCode)frame.fcode;
-    switch (fcode) {
-      // 读取线圈和保持寄存器，发送响应帧
-      case FunctionCode::READ_COILS:
-      case FunctionCode::READ_REGS:
-        sendResponseFrame(&frame);
-        break;
-      case FunctionCode::WRITE_COIL:
-      case FunctionCode::WRITE_COILS:
-      case FunctionCode::WRITE_REG:
-      case FunctionCode::WRITE_REGS:
-        setRegister(&frame, buffer);
-        break;
-    }
+    handleBuffer(&appSerial, APP_EN_PIN, app_buffer, app_buffer_index);
+    app_buffer_index = 0;
   }
 }
